a341f85b6645d6f1f3f7f07988b0182ef168c1fc
DeaconOrlov <deaconorlov@gmail.com>
2021-09-05 16:35:03 -0400

    add allocate seat method

--- airtravel.py
+++ airtravel.py
@@ -2,8 +2,9 @@
 
 
 class Flight: 
+    """A flight with a particular passenger airdraft."""
     
-    def __init__(self, number):
+    def __init__(self, number, aircraft):
         if not number[:2].isalpha():
             raise ValueError(f"No airline code in '{number}'")
 
@@ -14,6 +15,12 @@ class Flight:
             raise ValueError(f"Invalid route number '{number}'")
 
         self._number = number
+        self._aircraft = aircraft
+        rows, seats = self._aircraft.seating_plan()
+        self._seating = [None] + [{letter: None for letter in seats} for _ in rows]
+
+    def aircraft(self):
+        return self._aircraft.model()
 
     def number(self):
         return self._number
@@ -21,7 +28,33 @@ class Flight:
     def airline(self):
         return self._number[:2]
 
+    def allocate_seat(self, seat, passenger):
+        """allocate a seat to a passenger
+        
+        Args: A seat designator such as '12C' or '21F'.
+        
+        Raises:
+            ValueError: If the seat is unavailable.
+        """
+        rows, seat_letters = self._aircraft.seating_plan()
 
+        letter = seat[-1]
+        if letter not in seat_letters:
+            raise ValueError(f"Invalid seat letter {letter}")
+
+        row_text = seat[:-1]
+        try:
+            row = int(row_text)
+        except ValueError:
+            raise ValueError(f"Invalid seat row {row_text}")
+        
+        if row not in rows:
+            raise ValueError(f"Invalid row number {row}")
+
+        if self._seating[row][letter] is not None:
+            raise ValueError(f"Seat {seat} already occupied")
+
+        self._seating[row][letter] = passenger
 class Aircraft:
 
     def __init__(self, registration, model, num_rows, num_seats_per_row):
